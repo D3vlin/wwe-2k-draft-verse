2d659afe93b814792e02ed795656cd9cff8dd635
D3vlin <alexisdq16@gmail.com>
2024-11-10 18:16:56 -0500

    fix backround

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,9 +1,19 @@
 const Card = ({ image, media, name }) => {
+    const backgroundImage = import.meta.env.VITE_BACKGROUND_IMAGE_PATH
+
     return (
         <article className="cursor-pointer rounded-lg text-black w-[calc(100%/13)] bg-slate-800">
-            <figure className="relative w-full">
-                <img className="w-full h-full object-cover bg-[url('./Images/RosterWwe2k24/backgroundCard.webp')] bg-cover bg-center rounded-tl-lg rounded-tr-lg"
-                    src={image} />
+            <figure
+                className="relative w-full rounded-tl-lg rounded-tr-lg"
+                style={{
+                    backgroundImage: `url('${backgroundImage}')`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}>
+                <img
+                    className={`w-full h-full object-cover bg-cover bg-center`}
+                    src={image}
+                />
                 <figcaption className="absolute top-0 right-0 bg-slate-600 rounded-lg text-white text-xs px-0.5 py-0.5">{media}</figcaption>
             </figure>
             <p className="flex items-center justify-center text-center bg-slate-200 rounded-bl-lg rounded-br-lg h-[30px]">
