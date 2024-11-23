4f86e9880fac3c3dcea5755ee23d48a99f469f54
D3vlin <alexisdq16@gmail.com>
2024-11-23 09:12:13 -0500

    add toggle button

--- src/Components/RosterContainer/index.jsx
+++ src/Components/RosterContainer/index.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
+import { MinusCircleIcon, PlusCircleIcon } from "@heroicons/react/24/outline"
+
 const RosterContainer = ({ title, children }) => {
+    const [display, setDisplay] = useState(true)
+
+    const renderIcon = () => {
+        return display
+            ? <MinusCircleIcon className="h-6 w-6 absolute top-1/3 left-3 cursor-pointer text-violet-400 hover:text-white" onClick={() => setDisplay(!display)} />
+            : <PlusCircleIcon className="h-6 w-6 absolute top-1/3 left-3 cursor-pointer text-violet-400 hover:text-white" onClick={() => setDisplay(!display)} />
+    }
+
     return (
         <div className=" mt-3 w-full max-w-screen-xl">
-            <div className="flex flex-col items-center justify-center w-full bg-slate-700">
+            <div className="flex flex-col relative items-center justify-center w-full bg-slate-700">
                 <h2 className="font-medium text-lg mb-4 mt-4">{title}</h2>
+                {renderIcon()}
             </div>
-            <div className="flex flex-wrap items-center gap-2 w-full h-[350px] overflow-y-scroll bg-slate-800 p-3 rounded-lg">
+            <div className={`${display ? 'flex' : 'hidden'} flex-wrap items-center gap-2 w-full h-[350px] overflow-y-scroll bg-slate-800 p-3 rounded-lg`}>
                 {children}
             </div>
         </div>
